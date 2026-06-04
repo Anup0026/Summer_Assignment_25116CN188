876f01faf2e0e13e57f180ce71fb6d305037b685
Anup Yadav <anupyadav7409@gmail.com>
2026-06-04 16:41:16 +0530

    Dayeight_problem_Solutions

---
+++ Dayeight/Repeated_number_in_pyramid.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Repeated_number_in_pyramid {
+
+    public static void Repeated_number_in_pyramid(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number = ");
+        int n = sc.nextInt();
+
+        System.out.println("============== Repeated Pyramid is ============");
+
+        Repeated_number_in_pyramid(n);
+
+    }
+
+}
